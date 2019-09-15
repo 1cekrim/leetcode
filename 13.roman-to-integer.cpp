540be15cd95fb8928aa5cfc73b940f8318095cc0
HyeonSu Kim <rla92233@naver.com>
2019-09-15 13:28:14 +0900

    [13] Roman to Integer

---
+++ 13.roman-to-integer.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=13 lang=cpp
+ *
+ * [13] Roman to Integer
+ */
+#include <map>
+#include <string>
+using namespace std;
+
+class Solution
+{
+ public:
+    int romanToInt(string s)
+    {
+        static map<char, int> roman = { { 'I', 1 },   { 'V', 5 },
+                                        { 'X', 10 },  { 'L', 50 },
+                                        { 'C', 100 }, { 'D', 500 },
+                                        { 'M', 1000 } };
+        int result = 0;
+        for (size_t i = 0; i < s.length(); ++i)
+        {
+            if (roman[s[i]] < roman[s[i + 1]])
+            {
+                result -= roman[s[i]];
+            }
+            else
+            {
+                result += roman[s[i]];
+            }
+        }
+        return result;
+    }
+};
